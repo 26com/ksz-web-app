5d70a696e49d7af398fcc9b233f1abf23a142bd7
Vladimir Suhoivanenkov <26com@mail.ru>
2021-12-01 09:11:58 +0300

    Reviews bug fixed

--- src/routes/Reviews/Reviews.js
+++ src/routes/Reviews/Reviews.js
@@ -11,8 +11,8 @@ export function Reviews({content, size, setTabValue}) {
   return (
     <div className="reviews-route-container">
       {content.reviews.map(review => (
-        <div className="reviews-route-item">
-          <Review review={review} key={review.id}/>
+        <div className="reviews-route-item" key={review.id}>
+          <Review review={review} />
         </div>
         ))}
     </div>
