6af8c4ef9a5261fffd88518a3d6a0ea5f02ede92
Vladimir Suhoivanenkov <26com@mail.ru>
2021-12-02 11:59:47 +0300

    Preload Logo removed

--- src/components/Loader/Loader.js
+++ src/components/Loader/Loader.js
@@ -6,9 +6,6 @@ export function Loader(){
   return (
     <div className="loader-container">
       <div className="loader-content">
-        <div className="loader-logo">
-          <img src="https://www.doctorznaet.ru/manager/templates/doctorznaet/img/logo-medical.png" alt="logo"></img>
-        </div>
         <div className="loader-animation">
           <div className="lds-ellipsis"><div></div><div></div><div></div><div></div></div>
         </div>
--- src/components/carousels/UserCarousel/UsersCarousel.js
+++ src/components/carousels/UserCarousel/UsersCarousel.js
@@ -49,7 +49,7 @@ export function UsersCarousel({setUser}){
         {users.map(user => (
           <SplideSlide key={user.id}>
             <div className={"users-carousel-item " + size} key={user.id} onClick={() => {clickHandler(user.id)}}>
-              <img src={window.apiConfig.url + '/file/get-user-photo?idUser=' + user.id + '&size=middle'} alt="user"/><br />
+              <img src={window.apiConfig.url + '/file/get-user-photo?idUser=' + user.id} alt="user"/><br />
               <span className="user-name">{user.surname} {user.name} {user.middleName}</span>
               {user.specials && user.specials.map((special, index) => (
                 <div key={index}>
