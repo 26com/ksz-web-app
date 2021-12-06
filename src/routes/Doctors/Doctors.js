eed34053556c5c55b43c4c052d289edfa08ef0c5
Vladimir Suhoivanenkov <26com@mail.ru>
2021-12-06 15:28:58 +0300

    Users search input added

--- src/routes/Doctors/Doctors.js
+++ src/routes/Doctors/Doctors.js
@@ -3,12 +3,38 @@ import './style.css'
 import { useNavigate } from "react-router";
 
 export function Doctors({size, setTabValue, users, setUser}) {
+
+  const [doctors, setDoctors] = React.useState(users)
+  const input = React.useRef(null)
   const imgSize = '250px'
   const navigate = useNavigate()
   if (!Object.keys(users).length) navigate('/')
 
   window.scrollTo({ top: 0, behavior: 'smooth' })
 
+  const inputChangeHandler = (e) => {
+    const q = (e.target.value).toLowerCase()
+    const result = {}
+    for (let key in users) {
+      const user = users[key]
+      const fullName = (user.surname +  ' ' + user.name + ' ' + user.middleName).toLowerCase()
+      if (fullName.includes(q)) {
+        result[key] = user
+      }
+      else if (user.specials) {
+        user.specials.forEach(special => {
+          if (special.toLowerCase().includes(q)) {
+            result[key] = user
+          }
+        })
+      }
+    }
+    setDoctors(result)
+  }
+  const inputClearHandler = () => {
+    setDoctors(users)
+    input.current.value = ''
+  }
   const clickHandler = (id) => {
     setUser(id)
     navigate('/users')
@@ -20,7 +46,13 @@ export function Doctors({size, setTabValue, users, setUser}) {
   return (
     <>
     <div className="doctors-container">
-      {Object.keys(users).map(key => (
+      <div className="doctors-search" onChange={inputChangeHandler}>
+        <input type="text" placeholder="Введите ФИО врача | специальность" ref={input} />
+      </div>
+      <div className="input-clear-btn">
+        <i className="fas fa-times" onClick={inputClearHandler}></i>
+      </div>
+      {Object.keys(doctors).map(key => (
         <div style={{width: imgSize, margin: '15px 10px'}} className={"users-carousel-item " + size} key={users[key].id} onClick={() => {clickHandler(users[key].id)}}>
         <img 
           className="user-avatar"
