395d7d7ea396d371f7374ff37ec2779a9c872088
Vladimir Suhoivanenkov <26com@mail.ru>
2021-12-06 14:30:00 +0300

    Instagram news style changed

--- src/components/carousels/NewsCarousel/components/InstagramNews.js
+++ src/components/carousels/NewsCarousel/components/InstagramNews.js
@@ -8,17 +8,31 @@ import CustomButton from '../../../buttons/CustomButton/CustomButton';
 export function InstagramNews({news, size, id}){ 
   const [preview, setPreview] = useState(true)
   const myRef = useRef(null)
+  const instImg = useRef(null)
   const clickHandler = () => {
     setPreview(false)
   }
   useEffect(() => {
     if (myRef.current) myRef.current.play()
   }, [preview])
+  useEffect(() => {
+    if (instImg.current) {
+      const width = instImg.current.clientWidth
+      instImg.current.style.height = width + 'px'
+    }
+  }, [])
   return (
     <div className="news-carousel-item">
       {(news.instagramMediaType === 'IMAGE' || news.instagramMediaType === 'CAROUSEL_ALBUM') && 
         <Link to={'/news?id=' + id} className="list-item">
-          <img src={news.instagramMediaUrl} style={{width: '100%', borderTopLeftRadius: '50px', borderTopRightRadius: '50px'}} alt="news-media"></img>
+          <img 
+            src={news.instagramMediaUrl} 
+            style={{width: '100%', borderTopLeftRadius: '50px', borderTopRightRadius: '50px'}} 
+            alt="news-media"
+            className="instagramm-img"
+            ref={instImg}
+          >
+          </img>
         </Link>
       }
       {news.instagramMediaType === 'VIDEO' &&
