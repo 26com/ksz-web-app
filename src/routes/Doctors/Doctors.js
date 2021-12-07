5f3e85f35e3acf6e94301f59aac34be490ff163e
Vladimir Suhoivanenkov <26com@mail.ru>
2021-12-07 16:44:46 +0300

    Users highlighting the request whith color added

--- src/routes/Doctors/Doctors.js
+++ src/routes/Doctors/Doctors.js
@@ -13,23 +13,33 @@ export function Doctors({size, setTabValue, users, setUser}) {
   window.scrollTo({ top: 0, behavior: 'smooth' })
 
   const inputChangeHandler = (e) => {
+    if (!e.target.value) {
+      setDoctors({...users})
+      return
+    }
     const q = (e.target.value).toLowerCase()
     const result = {}
     for (let key in users) {
-      const user = users[key]
-      const fullName = (user.surname +  ' ' + user.name + ' ' + user.middleName).toLowerCase()
-      if (fullName.includes(q)) {
-        result[key] = user
+      const user = {...users[key]}
+      let fullName = (user.surname +  ' ' + user.name + ' ' + user.middleName)
+      if (fullName.toLowerCase().includes(q)) {
+        fullName = fullName.replace(q, `<span class="primary-color">${q}</span>`)
+        fullName = fullName.replace(q[0].toUpperCase() + q.slice(1), `<span class="primary-color">${q[0].toUpperCase() + q.slice(1)}</span>`)
+        result[key] = {...user}
+        result[key].fullName = fullName
       }
       else if (user.specials) {
-        user.specials.forEach(special => {
+        user.specials.forEach((special, index) => {
           if (special.toLowerCase().includes(q)) {
-            result[key] = user
+            result[key] = {...user}
+            result[key].specials = [...(user.specials)]
+            result[key].specials[index] = result[key].specials[index].replace(q, `<span class="primary-color">${q}</span>`)
+            result[key].specials[index] = result[key].specials[index].replace(q[0].toUpperCase() + q.slice(1), `<span class="primary-color">${q[0].toUpperCase() + q.slice(1)}</span>`)
           }
         })
       }
     }
-    setDoctors(result)
+    setDoctors({...result})
   }
   const inputClearHandler = () => {
     setDoctors(users)
@@ -58,7 +68,12 @@ export function Doctors({size, setTabValue, users, setUser}) {
         </div>
       }
       {Object.keys(doctors).map(key => (
-        <div style={{width: imgSize, margin: '15px 10px'}} className={"users-carousel-item " + size} key={users[key].id} onClick={() => {clickHandler(users[key].id)}}>
+        <div 
+          style={{width: imgSize, margin: '15px 10px'}} 
+          className={"users-carousel-item " + size} 
+          key={users[key].id} 
+          onClick={() => {clickHandler(users[key].id)}}
+        >
         <img 
           className="user-avatar"
           style={{minHeight: imgSize}}
@@ -66,10 +81,24 @@ export function Doctors({size, setTabValue, users, setUser}) {
           alt="user"
         />
         <br />
-        <span className="user-name">{users[key].surname} {users[key].name} {users[key].middleName}</span>
-        {users[key].specials && users[key].specials.map((special, index) => (
+        {!doctors[key].fullName && 
+          <span className="user-name">
+            {users[key].surname} {users[key].name} {users[key].middleName}
+          </span>
+        }
+        {doctors[key].fullName && 
+          <div 
+            dangerouslySetInnerHTML={{ __html: doctors[key].fullName}} 
+            className="user-name">
+          </div>
+        }
+        {doctors[key].specials && doctors[key].specials.map((special, index) => (
           <div key={index}>
-            <span className="user-special">{special}</span>
+            {/* <span className="user-special">{special}</span> */}
+              <div 
+              dangerouslySetInnerHTML={{ __html: special}} 
+              className="user-special">
+            </div>
           </div>
         ))}
       </div>
